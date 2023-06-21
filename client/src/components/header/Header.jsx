ef25fdaf9f3609812736dcaa650cd8a9f790f1d7
139Vaibhav <vaibhavkumarsingh139@gmail.com>
2023-06-21 23:08:09 +0530

    Ready for Deploy

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">BlogO</span>
+      </div>
+    </div>
+  );
+}
